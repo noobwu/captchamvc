2f2fab3c48fef1e27400722fb19f398965c9eb66
VVS0205 <VVS0205@VVS0205-PC>
2012-07-20 18:52:15 +0800

    Removed old code of the project.

--- IEncryption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CaptchaMVC
-{
-    public interface IEncryption
-    {
-        string Encrypt(string inputText, string password, byte[] salt);
-
-        string Decrypt(string inputText, string password, byte[] salt);
-    }
-}
