9a9c20b488322b46f40da705b7b25685beca63cb
VVS0205 <VVS0205@VVS0205-PC>
2012-07-23 20:12:50 +0800

    Added JetBrains annotations attributes.

---
+++ CaptchaMvc(Mvc 3)/Annotations/JetBrains.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Copyright 2007-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+// ReSharper disable CheckNamespace
+
+namespace JetBrains.Annotations
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// Indicates that the marked symbol is used implicitly (e.g. via reflection, in external library),
+    /// so this symbol will not be marked as unused (as well as by other usage inspections)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+    public sealed class UsedImplicitlyAttribute : Attribute
+    {
+        [UsedImplicitly]
+        public UsedImplicitlyAttribute()
+            : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default)
+        {
+        }
+
+        [UsedImplicitly]
+        public UsedImplicitlyAttribute(ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
+        {
+            UseKindFlags = useKindFlags;
+            TargetFlags = targetFlags;
+        }
+
+        [UsedImplicitly]
+        public UsedImplicitlyAttribute(ImplicitUseKindFlags useKindFlags)
+            : this(useKindFlags, ImplicitUseTargetFlags.Default)
+        {
+        }
+
+        [UsedImplicitly]
+        public UsedImplicitlyAttribute(ImplicitUseTargetFlags targetFlags)
+            : this(ImplicitUseKindFlags.Default, targetFlags)
+        {
+        }
+
+        [UsedImplicitly]
+        public ImplicitUseKindFlags UseKindFlags { get; private set; }
+
+        /// <summary>
+        /// Gets value indicating what is meant to be used
+        /// </summary>
+        [UsedImplicitly]
+        public ImplicitUseTargetFlags TargetFlags { get; private set; }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class PathReferenceAttribute : Attribute
+    {
+        public PathReferenceAttribute()
+        {
+        }
+
+        [UsedImplicitly]
+        public PathReferenceAttribute([PathReference] string basePath)
+        {
+            BasePath = basePath;
+        }
+
+        [UsedImplicitly]
+        public string BasePath { get; private set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Method)]
+    public sealed class AspMvcPartialViewAttribute : PathReferenceAttribute
+    {
+    }
+
+    [Flags]
+    public enum ImplicitUseKindFlags
+    {
+        Default = Access | Assign | InstantiatedWithFixedConstructorSignature,
+
+        /// <summary>
+        /// Only entity marked with attribute considered used
+        /// </summary>
+        Access = 1,
+
+        /// <summary>
+        /// Indicates implicit assignment to a member
+        /// </summary>
+        Assign = 2,
+
+        /// <summary>
+        /// Indicates implicit instantiation of a type with fixed constructor signature.
+        /// That means any unused constructor parameters won't be reported as such.
+        /// </summary>
+        InstantiatedWithFixedConstructorSignature = 4,
+
+        /// <summary>
+        /// Indicates implicit instantiation of a type
+        /// </summary>
+        InstantiatedNoFixedConstructorSignature = 8,
+    }
+
+    /// <summary>
+    /// Specify what is considered used implicitly when marked with <see cref="MeansImplicitUseAttribute"/> or <see cref="UsedImplicitlyAttribute"/>
+    /// </summary>
+    [Flags]
+    public enum ImplicitUseTargetFlags
+    {
+        Default = Itself,
+
+        Itself = 1,
+
+        /// <summary>
+        /// Members of entity marked with attribute are considered used
+        /// </summary>
+        Members = 2,
+
+        /// <summary>
+        /// Entity marked with attribute and all its members considered used
+        /// </summary>
+        WithMembers = Itself | Members
+    }
+}
+
+
