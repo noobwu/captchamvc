633130fea648f036a6231027fe1eff1a5c8b2538
VVS0205 <VVS0205@VVS0205-PC>
2012-07-23 20:14:00 +0800

    Added the PartialCaptchaBuilder for partial captcha view.

---
+++ CaptchaMvc(Mvc 3)/Infrastructure/PartialCaptchaBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using CaptchaMvc.Interface;
+using CaptchaMvc.Models;
+
+namespace CaptchaMvc.Infrastructure
+{
+    /// <summary>
+    /// Implementation of a <see cref="ICaptchaBulder"/> for build partial captcha.
+    /// </summary>
+    public class PartialCaptchaBuilder : ICaptchaBulder
+    {
+        #region Implementation of ICaptchaBulder
+
+        /// <summary>
+        /// Create a new captcha to the specified <see cref="IBuildInfoModel"/>.
+        /// </summary>
+        /// <param name="buildInfoModel">The specified <see cref="IBuildInfoModel"/>.</param>
+        /// <returns>The html string with the captcha.</returns>
+        public MvcHtmlString Build(IBuildInfoModel buildInfoModel)
+        {
+            var infoModel = buildInfoModel as PartialBuildInfoModel;
+            if (infoModel == null)
+                throw new ArgumentException("A PartialCaptchaBuilder can only work with the PartialBuildInfoModel.");
+            if (infoModel.ViewData != null)
+                return infoModel.HtmlHelper.Partial(infoModel.PartialViewName, infoModel.BuildInfoModel,
+                                                    infoModel.ViewData);
+            return infoModel.HtmlHelper.Partial(infoModel.PartialViewName, infoModel.BuildInfoModel);
+        }
+
+        #endregion
+    }
+}
