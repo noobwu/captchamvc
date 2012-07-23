876935662e5f59492cf12e313c8aabd5455718f2
VVS0205 <VVS0205@VVS0205-PC>
2012-07-23 20:14:40 +0800

    Added the PartialBuildInfoModel for partial captcha view.

---
+++ CaptchaMvc(Mvc 3)/Models/PartialBuildInfoModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Web.Mvc;
+using CaptchaMvc.Interface;
+
+namespace CaptchaMvc.Models
+{
+    /// <summary>
+    /// Represents the default model with information for create a partial captcha.
+    /// </summary>
+    public class PartialBuildInfoModel : IBuildInfoModel
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PartialBuildInfoModel"/> class.
+        /// </summary>
+        public PartialBuildInfoModel(HtmlHelper htmlHelper, IBuildInfoModel buildInfoModel, string partialViewName,
+                                     ViewDataDictionary viewData)
+        {
+            HtmlHelper = htmlHelper;
+            BuildInfoModel = buildInfoModel;
+            PartialViewName = partialViewName;
+            ViewData = viewData;
+        }
+
+        #endregion
+
+        #region Implementation of IBuildInfoModel
+
+        /// <summary>
+        /// Gets the specified <see cref="IBuildInfoModel"/> for create captcha.
+        /// </summary>
+        public IBuildInfoModel BuildInfoModel { get; set; }
+
+        /// <summary>
+        /// Gets the specified partial view name.
+        /// </summary>
+        public string PartialViewName { get; set; }
+
+        /// <summary>
+        /// Gets the specified <see cref="ViewDataDictionary"/>.
+        /// </summary>
+        public ViewDataDictionary ViewData { get; set; }
+
+        /// <summary>
+        /// Gets the token parameter name.
+        /// </summary>
+        public string TokenParameterName
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the required field message.
+        /// </summary>
+        public string RequiredMessage
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the is required flag.
+        /// </summary>
+        public bool IsRequired
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the refresh button text.
+        /// </summary>
+        public string RefreshButtonText
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the input text.
+        /// </summary>
+        public string InputText
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the specified <see cref="IBuildInfoModel.HtmlHelper"/>.
+        /// </summary>
+        public HtmlHelper HtmlHelper { get; private set; }
+
+        /// <summary>
+        /// Gets the input element id in DOM.
+        /// </summary>
+        public string InputElementId
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the token element id in DOM.
+        /// </summary>
+        public string TokenElementId
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the image element id in DOM.
+        /// </summary>
+        public string ImageElementId
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the image url.
+        /// </summary>
+        public string ImageUrl
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the refresh url.
+        /// </summary>
+        public string RefreshUrl
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the token value.
+        /// </summary>
+        public string TokenValue
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        #endregion
+    }
+}
