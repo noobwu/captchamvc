da0448aee431a3a1867bd780a172e8a13813d513
VVS0205 <VVS0205@VVS0205-PC>
2013-01-25 00:40:47 +0800

    Captcha can split markup and script generation. Added factories in CaptchaUtils to create ICaptchManager, ICaptchaBuilderProvider, IGenerateImage, now you can have multiple instances of these objects. Extend methods for creating captcha. Replaced DefaultGenerateImage, previous implementation was very slow. Clean up code. Minor changes.

---
+++ CaptchaMvc(Mvc 3)/Interface/ICaptcha.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace CaptchaMvc.Interface
+{
+    /// <summary>
+    ///     Represents the captcha model.
+    /// </summary>
+    public interface ICaptcha : IHtmlString
+    {
+        /// <summary>
+        ///     Renders only captcha markup, if any.
+        /// </summary>
+        /// <returns>
+        ///     An instance of <see cref="IHtmlString" />.
+        /// </returns>
+        IHtmlString RenderMarkup();
+
+        /// <summary>
+        ///     Renders only captcha scripts, if any.
+        /// </summary>
+        /// <returns>
+        ///     An instance of <see cref="IHtmlString" />.
+        /// </returns>
+        IHtmlString RenderScript();
+    }
+}
---
+++ CaptchaMvc(Mvc 3)/Models/CaptchaModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using CaptchaMvc.Interface;
+
+namespace CaptchaMvc.Models
+{
+    /// <summary>
+    ///     Represents the default captcha model.
+    /// </summary>
+    public class CaptchaModel : ICaptcha
+    {
+        #region Fields
+
+        private readonly string _markup;
+        private readonly string _script;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CaptchaModel" /> class.
+        /// </summary>
+        public CaptchaModel(string markup, string script)
+        {
+            if (markup == null) throw new ArgumentNullException("markup");
+            _markup = markup;
+            if (script == null)
+                script = string.Empty;
+            _script = script;
+        }
+
+        #endregion
+
+        #region Implementation of IHtmlString
+
+        /// <summary>
+        ///     Returns an HTML-encoded string.
+        /// </summary>
+        /// <returns>
+        ///     An HTML-encoded string.
+        /// </returns>
+        public string ToHtmlString()
+        {
+            if (string.IsNullOrEmpty(_script))
+                return _markup;
+            return string.Format("{0} {1}", _script, _markup);
+        }
+
+        #endregion
+
+        #region Implementation of ICaptcha
+
+        /// <summary>
+        ///     Renders only captcha markup, if any.
+        /// </summary>
+        /// <returns>
+        ///     An instance of <see cref="IHtmlString" />.
+        /// </returns>
+        public IHtmlString RenderMarkup()
+        {
+            return new MvcHtmlString(_markup);
+        }
+
+        /// <summary>
+        ///     Renders only captcha scripts, if any.
+        /// </summary>
+        /// <returns>
+        ///     An instance of <see cref="IHtmlString" />.
+        /// </returns>
+        public IHtmlString RenderScript()
+        {
+            return new MvcHtmlString(_script);
+        }
+
+        #endregion
+    }
+}
